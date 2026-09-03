e8f4b7bd22f65e45b19cd4e67cd0c1a982109102
6Morpheus6 <wanton-decay@gmx.net>
2026-09-03 05:45:25 +0200

    update
    - update torch.js
    - bluefairy fix

--- torch.js
+++ torch.js
@@ -1,94 +1,84 @@
 module.exports = {
   run: [
-    // windows nvidia
+    // nvidia windows 
     {
-      when: "{{platform === 'win32' && gpu === 'nvidia'}}",
-      method: "shell.run",
-      params: {
-        venv: "{{args && args.venv ? args.venv : null}}",
-        path: "{{args && args.path ? args.path : '.'}}",
-        message: [
-          "uv pip install numpy==1.26.4",
-          "uv pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128"
-        ]
-      }
+      "when": "{{gpu === 'nvidia' && platform === 'win32'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps"
+      },
+      "next": null
     },
-    // windows amd
+    // nvidia linux
     {
-      when: "{{platform === 'win32' && gpu === 'amd'}}",
-      method: "shell.run",
-      params: {
-        venv: "{{args && args.venv ? args.venv : null}}",
-        path: "{{args && args.path ? args.path : '.'}}",
-        message: [
-          "uv pip install numpy==1.26.4",
-          "uv pip install torch-directml torchaudio torchvision"
-        ]
-      }
+      "when": "{{gpu === 'nvidia' && platform === 'linux'}}",
+      "method": "shell.run",
+      "params": {
+        "bluefairy": "off",
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu128 --force-reinstall"
+      },
+      "next": null
     },
-    // windows cpu
+    // amd windows
     {
-      when: "{{platform === 'win32' && (gpu !== 'nvidia' && gpu !== 'amd')}}",
-      method: "shell.run",
-      params: {
-        venv: "{{args && args.venv ? args.venv : null}}",
-        path: "{{args && args.path ? args.path : '.'}}",
-        message: [
-          "uv pip install numpy==1.26.4",
-          "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1"
-        ]
-      }
+      "when": "{{gpu === 'amd' && platform === 'win32'}}",
+      "method": "shell.run",
+      "params": {
+        "bluefairy": "off",
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install torch-directml torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 numpy==1.26.4 --force-reinstall"
+      },
+      "next": null
     },
-    // mac
+    // amd linux (rocm)
     {
-      when: "{{platform === 'darwin'}}",
-      method: "shell.run",
-      params: {
-        venv: "{{args && args.venv ? args.venv : null}}",
-        path: "{{args && args.path ? args.path : '.'}}",
-        message: [
-          "uv pip install numpy==1.26.4",
-          "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1"
-        ]
-      }
+      "when": "{{gpu === 'amd' && platform === 'linux'}}",
+      "method": "shell.run",
+      "params": {
+        "bluefairy": "off",
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/rocm6.3 --force-reinstall"
+      },
+      "next": null
     },
-    // linux nvidia
+    // apple silicon mac
     {
-      when: "{{platform === 'linux' && gpu === 'nvidia'}}",
-      method: "shell.run",
-      params: {
-        venv: "{{args && args.venv ? args.venv : null}}",
-        path: "{{args && args.path ? args.path : '.'}}",
-        message: [
-          "uv pip install numpy==1.26.4",
-          "uv pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128"
-        ]
-      }
+      "when": "{{platform === 'darwin' && arch === 'arm64'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cpu --force-reinstall --no-deps"
+      },
+      "next": null
     },
-    // linux rocm (amd)
+    // intel mac
     {
-      when: "{{platform === 'linux' && gpu === 'amd'}}",
-      method: "shell.run",
-      params: {
-        venv: "{{args && args.venv ? args.venv : null}}",
-        path: "{{args && args.path ? args.path : '.'}}",
-        message: [
-          "uv pip install numpy==1.26.4",
-          "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/rocm6.0"
+      "when": "{{platform === 'darwin' && arch !== 'arm64'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": [
+          "uv pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cpu --force-reinstall --no-deps",
+          "uv pip install numpy==1.26.4"
         ]
-      }
+      },
+      "next": null
     },
-    // linux cpu
+    // cpu
     {
-      when: "{{platform === 'linux' && (gpu !== 'amd' && gpu !=='nvidia')}}",
-      method: "shell.run",
-      params: {
-        venv: "{{args && args.venv ? args.venv : null}}",
-        path: "{{args && args.path ? args.path : '.'}}",
-        message: [
-          "uv pip install numpy==1.26.4",
-          "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu"
-        ]
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0  --index-url https://download.pytorch.org/whl/cpu --force-reinstall --no-deps"
       }
     }
   ]
